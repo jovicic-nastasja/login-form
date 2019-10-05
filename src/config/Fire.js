6a4da3a7822e8375d75c7b63faf6fcd300069049
Nastasja Jovicic <jovicic.nastasja@gmail.com>
2019-10-05 18:45:35 +0300

    updated firebase conf

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // Firebase configuration settings
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
-  };
+  apiKey: "AIzaSyBDGQY6xt6XhJHiXdNGSSgW0NF4wiKIFiA",
+  authDomain: "login-form-82502.firebaseapp.com",
+  databaseURL: "https://login-form-82502.firebaseio.com",
+  projectId: "login-form-82502",
+  storageBucket: "login-form-82502.appspot.com",
+  messagingSenderId: "351591800454"
+};
 const fire = firebase.initializeApp(config);
 export default fire;
